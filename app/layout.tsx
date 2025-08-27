212ae43ea88728331e5385e46649414bffafa45c
Binyam Cheru <binyamcheru123@gmail.com>
2025-08-27 18:55:27 +0300

    add font and see how to use fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono as GeistMono } from "next/font/google";
+import { Inter, Lobster } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
+  variable: "--font-inter", // expose Inter as a CSS variable
 });
 
-const geistMono = GeistMono({
-  variable: "--font-geist-mono",
+const lobster = Lobster({
+  weight: "400",
   subsets: ["latin"],
+  variable: "--font-lobster",
 });
 
 export const metadata: Metadata = {
@@ -24,9 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} ${lobster.variable} antialiased`}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <h1>Next.js App</h1>
+    <>
+      <h1 className="font-inter text-3xl">Hello with Inter</h1>
+      <h1 className="font-lobster text-3xl">Hello with Inter</h1>
+      <p>This paragraph uses the default font.</p>
+    </>
   );
 }
