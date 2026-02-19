8756961f89e2aa15f38789d862c4c1f45865a2ad
Binyam Cheru <binyamcheru123@gmail.com>
2026-02-19 21:24:07 +0300

    implement displaying tags

--- components/search/LocalSearch.tsx
+++ components/search/LocalSearch.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
-import { formUrlQuery, removeKeysFormUrlQuery } from "@/lib/urls";
 import Image from "next/image";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
-interface LocalSearchProps {
+import { formUrlQuery, removeKeysFormUrlQuery } from "@/lib/urls";
+
+import { Input } from "../ui/input";
+
+interface Props {
   route: string;
   imgSrc: string;
   placeholder: string;
   otherClasses?: string;
+  iconPosition?: "left" | "right";
 }
 
 const LocalSearch = ({
@@ -18,24 +21,24 @@ const LocalSearch = ({
   imgSrc,
   placeholder,
   otherClasses,
-}: LocalSearchProps) => {
+  iconPosition = "left",
+}: Props) => {
   const pathname = usePathname();
-  console.log(pathname);
-  const router = useRouter(); // common mistake useRouter comes from next/navigation not from next/router
+  const router = useRouter();
   const searchParams = useSearchParams();
   const query = searchParams.get("query") || "";
-  console.log(searchParams.toString());
 
   const [searchQuery, setSearchQuery] = useState(query);
 
   useEffect(() => {
-    const debounceFn = setTimeout(() => {
+    const delayDebounceFn = setTimeout(() => {
       if (searchQuery) {
         const newUrl = formUrlQuery({
           params: searchParams.toString(),
           key: "query",
           value: searchQuery,
         });
+
         router.push(newUrl, { scroll: false });
       } else {
         if (pathname === route) {
@@ -43,25 +46,29 @@ const LocalSearch = ({
             params: searchParams.toString(),
             keysToRemove: ["query"],
           });
+
           router.push(newUrl, { scroll: false });
         }
       }
     }, 300);
 
-    return () => clearTimeout(debounceFn);
-  }, [searchQuery, router, searchParams, route, pathname]);
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchQuery, router, route, searchParams, pathname]);
 
   return (
     <div
-      className={`background-light800_darkgradient flex min-h-[56px] items-center gap-4 rounded-[10px] px-4 grow ${otherClasses} `}
+      className={`background-light800_darkgradient flex min-h-14 grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
     >
-      <Image
-        src={imgSrc}
-        width={24}
-        height={24}
-        alt="Search"
-        className="cursor-pointer"
-      />
+      {iconPosition === "left" && (
+        <Image
+          src={imgSrc}
+          width={24}
+          height={24}
+          alt="Search"
+          className="cursor-pointer"
+        />
+      )}
+
       <Input
         type="text"
         placeholder={placeholder}
@@ -69,7 +76,18 @@ const LocalSearch = ({
         onChange={(e) => setSearchQuery(e.target.value)}
         className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
       />
+
+      {iconPosition === "right" && (
+        <Image
+          src={imgSrc}
+          width={15}
+          height={15}
+          alt="Search"
+          className="cursor-pointer"
+        />
+      )}
     </div>
   );
 };
+
 export default LocalSearch;
