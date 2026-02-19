297c7595d912d9ea844a88aa9563bc2a07881909
Binyam Cheru <binyamcheru123@gmail.com>
2026-02-19 20:32:13 +0300

    render ui states example when there is  no question

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -1,8 +1,11 @@
-import ROUTES from "@/constants/routes";
-import Link from "next/link";
-import { Badge } from "@/components/ui/badge";
-import { getDeviconClassName } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import ROUTES from "@/constants/routes";
+import { getDeviconClassName } from "@/lib/utils";
+
+import { Badge } from "../ui/badge";
 
 interface Props {
   _id: string;
@@ -30,6 +33,7 @@ const TagCard = ({
   const handleClick = (e: React.MouseEvent) => {
     e.preventDefault();
   };
+
   const Content = (
     <>
       <Badge className="subtle-medium background-light800_dark300 text-light400_light500 flex flex-row gap-2 rounded-md border-none px-4 py-2 uppercase">
@@ -62,7 +66,7 @@ const TagCard = ({
         {Content}
       </button>
     ) : (
-      <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
+      <Link href={ROUTES.TAG(_id)} className="flex justify-between gap-2">
         {Content}
       </Link>
     );
